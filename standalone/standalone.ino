a75577ea81112048d2c52d96cc32d7fd68f4140a
Christopher Chavez <chrischavez@gmx.us>
2015-12-24 13:43:58 -0600

    fix increment direction

--- standalone/standalone.ino
+++ standalone/standalone.ino
@@ -175,7 +175,7 @@ uint8_t readHue() {
 //(Why 32? maximimum number that fit inside signed 16-bit, and still fast)
 int photogateAverage() {
   int average = 0;
-  for(int count = 0; count < 32; count--) {
+  for(int count = 0; count < 32; count++) {
     average += analogRead(PHOTOGATE_PIN);
   }
   return average;
