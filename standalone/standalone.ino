3ffbcf1d43a3e8dfa6c7a091ef184dbff684a3a5
Christopher Chavez <chrischavez@gmx.us>
2016-01-14 11:49:17 -0600

    is it waiting for data?

--- standalone/standalone.ino
+++ standalone/standalone.ino
@@ -400,6 +400,8 @@ void gyroAngle(float target, bool is_counter_clockwise) {
       Serial.print("\trate: ");
       Serial.println(rate);
     } //end if
+    else
+      Serial.println("No data");
   } // end while
 
   //set gyro to bypass mode (disable FIFO)
