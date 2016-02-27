73ef6de24652f1f0547fc7935e69393620640870
Christopher Chavez <chrischavez@gmx.us>
2016-02-27 11:45:31 -0600

    average infrared readings

--- main/infrared.ino
+++ main/infrared.ino
@@ -1,7 +1,13 @@
 //Based on sketch by Noah Stahl (2011) for Sharp GP2Y0A02YK0F
 float read_ir_long_range_cm(int sensor_pin){
-	float sensorValue = analogRead(sensor_pin);
-	//inches = 4192.936 * pow(sensorValue,-0.935) - 3.937;
-	float cm = 10650.08 * pow(sensorValue,-0.935) - 10;
+	int32_t avg_sensor_value = 0;
+	int i;
+	const int N = 160;
+	for (i = 0; i < N; i++){
+		avg_sensor_value += analogRead(sensor_pin);
+		//delay(48);
+	}
+	//inches = 4192.936 * pow(((float)avg_sensor_value)/N,-0.935) - 3.937;
+	float cm = 10650.08 * pow(((float)avg_sensor_value)/N,-0.935) - 10;
 	return cm;
 }
