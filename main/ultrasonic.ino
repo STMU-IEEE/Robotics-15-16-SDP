ec61b9b4e8216fbda8a76750c086cb4533f7981c
Christopher Chavez <chrischavez@gmx.us>
2016-03-10 13:06:58 -0600

    add find_opening_offset_helper()

--- main/ultrasonic.ino
+++ main/ultrasonic.ino
@@ -96,3 +96,57 @@ void wallFollower(NewPing& srf_front, NewPing& srf_center){
   ST.stop();
 }
 
+
+/*
+Put the robot centered with opening facing toward it.
+The robot will point turn left, and then measure the forward encoder count to the wall,
+the reverse to find the wall on other side of opening.
+The robot will report the encoder counts for both, and the average
+(which is the offset to use in findOpening).
+
+03/10/16: typical value for 12 inch opening is -582
+
+*/  
+int32_t find_opening_offset_helper() {
+	Serial.println("find opening offset helper");
+	Serial.println("turning left...");
+	angle = 0;
+	ST.drive(0);
+	ST.turn(-10);
+	gyroAngle(-90);
+	ST.stop();
+	Serial.println("zeroing encoder...");
+	motor_R_encoder.write(0);
+	gyro_PID_setpoint = -90;
+	Serial.print("front wall count: ");
+	ST.drive(10);
+	do {
+		unsigned long timeNow = millis();
+		if(timeNow - last_SRF_trigger >= 50){
+			last_SRF_trigger = timeNow;
+			last_SRF_R_echo = srf_R.ping();
+		}
+		followGyro();
+	} while(srf_R.convert_cm(last_SRF_R_echo) > 30);
+	ST.stop();
+	int32_t front_wall_count = motor_R_encoder.read();
+	Serial.println(front_wall_count);
+	Serial.print("rear wall count: ");
+	ST.drive(-10);
+	do {
+		unsigned long timeNow = millis();
+		if(timeNow - last_SRF_trigger >= 50){
+			last_SRF_trigger = timeNow;
+			last_SRF_R_echo = srf_R.ping();
+		}
+		followGyro();
+	} while((motor_R_encoder.read() > 0)
+			|| (srf_R.convert_cm(last_SRF_R_echo) > 30));
+	ST.stop();
+	int32_t rear_wall_count = motor_R_encoder.read();
+	Serial.println(rear_wall_count);
+	Serial.print("average count: ");
+	int32_t average_count = (front_wall_count + rear_wall_count) / 2;
+	Serial.println(average_count);
+	return average_count;
+}
